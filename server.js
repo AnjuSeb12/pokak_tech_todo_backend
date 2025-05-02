5c3ee02303b54173d0bb6bf59f07f63c717de0d6
Anju Sebastian <christeenaanju@gmail.com>
2025-05-02 14:34:54 +0530

    updated cors

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const corsOptions = {
+  origin: 'https://pokak-todo.netlify.app',
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
-app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
